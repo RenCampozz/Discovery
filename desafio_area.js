52cce6076f5d24204e1de92aa89fc9b2df13eeb6
RenCampozz <108337893+RenCampozz@users.noreply.github.com>
2022-07-01 07:36:20 -0300

    Add files via upload

---
+++ desafio_area.js
@@ -0,0 +1,7 @@
+// PI*RAIO*RAIO
+
+const PI = 3.1415
+const RAIO = 10
+var ResultadodeArea = PI * RAIO * RAIO
+
+console.log ("area = " +ResultadodeArea+"m2.")
---
+++ desafio_trocadevalores.js
@@ -0,0 +1,11 @@
+var a = 7;
+var b = 94; 
+
+console.log (a)
+console.log (b)
+// Troca de valores 
+a = 94;
+b = 7;
+
+console.log(a)
+console.log(b)
